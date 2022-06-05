f6e674269b443f5c116338685697f8b8c175347f
Maaz Karim <MaazKarim02@gmail.com>
2022-06-05 19:15:04 +0530

    Initial Changes

---
+++ README.md
@@ -0,0 +1,2 @@
+## Exploratory Data Analysis in Python
+A simple application made using streamlit where one can go and do EDA of a given dataset .
---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+# import matplotlib as plt
+# import pandas_profiling import ProfileReport
+
+#Title
+st.markdown('''
+## **Exploratory Data Analysis with Python**
+### So what is EDA ?
+- Exploratory Data Analysis is a part in Data Analysis of performing initial investigations in data in order to discover patterns , check for outliers or to make hypothesis and check assumptions with the **power** of Statistics and graphical representations.
+''')
+
